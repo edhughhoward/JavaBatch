fa47403d40a4b2063428576a508d237511b3ec92
edhughhoward <edhughhoward@gmail.com>
2020-11-12 21:03:44 -0500

    11122020

---
+++ JavaReview/src/com/syntax/class01/DataTypesRecap.java
@@ -0,0 +1,36 @@
+package com.syntax.class01;
+
+public class DataTypesRecap {
+	
+	public static void main(String [] args) {
+		/*2 data types in java
+		 * Primitives + Non-primitives
+		 * 8 primitives
+		 * 	Numeric:
+		 * 		whole numbers (10) ==> byte, short, int, long
+		 * 		decimal numbers (10.99) ==> float double
+		 * Non-numeric:
+		 * 		single character ==> char
+		 * 		yes or no ==> boolean (true, false)
+		 * 
+		 * Variables are a container that hold values
+		 * 1. Type, 2. Name 3. Value
+		 */
+		
+		byte b = 10;
+		short short1 = 240;
+		int int1 = 112233;
+		long long1 = 1122334455l;
+		
+		double d = 10.99;
+		float _variable2 = 9.99f;
+		
+		char chargrade = 'B';
+		char chargrade = 'B';
+		
+		boolean booleanSnow = false;
+		
+		
+		
+	}
+}
